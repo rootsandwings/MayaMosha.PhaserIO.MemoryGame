e317cc7e20710782b4b594fa6df8dc94c2ecd4b7
GD <dixit_gaurav@yahoo.com>
2021-07-16 17:57:55 +0400

    All JSON related changes

--- scripts/dictionary/hindi/image.js
+++
@@ -1,6 +0,0 @@
-//GLOBAL Variable
-let IDict = {}
-
-IDict["lamp"] = { "ID": "1", "IMAGE": "assets/images/memory/lamp.png", "AUDIO": ""}
-IDict["mosha"] = { "ID": "2", "IMAGE": "assets/images/memory/mosha.png", "AUDIO": ""}
-IDict["josh"] = { "ID": "3", "IMAGE": "assets/images/memory/josh.png", "AUDIO": ""}
--- scripts/dictionary/image.js
+++ scripts/dictionary/image.js
@@ -5,9 +5,15 @@ let IDict = {}
 //Hindi
 //----------------------------//
 IDict["hindi"] = {}
+IDict["hindi"]["lamp"] = { "ID": "1", "IMAGE": "assets/images/memory/lamp.png", "AUDIO": ""}
+IDict["hindi"]["mosha"] = { "ID": "2", "IMAGE": "assets/images/memory/mosha.png", "AUDIO": ""}
+IDict["hindi"]["josh"] = { "ID": "3", "IMAGE": "assets/images/memory/josh.png", "AUDIO": ""}
 
 
 //----------------------------//
 //Telugu
 //----------------------------//
 IDict["telugu"] = {}
+IDict["telugu"]["lamp"] = { "ID": "1", "IMAGE": "assets/images/memory/lamp.png", "AUDIO": ""}
+IDict["telugu"]["mosha"] = { "ID": "2", "IMAGE": "assets/images/memory/mosha.png", "AUDIO": ""}
+IDict["telugu"]["josh"] = { "ID": "3", "IMAGE": "assets/images/memory/josh.png", "AUDIO": ""}
